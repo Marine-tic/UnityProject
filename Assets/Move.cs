65cd9002559f920f1f9af2255cb852418cd9a5e8
Marine Landraudie <marine.landraudie.tic@gmail.com>
2017-03-27 22:13:03 +0200

    Adding All starting file of the project

---
+++ Assets/Move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.forward * .05f * Time.deltaTime);
+        transform.Rotate(Vector3.up * 5f * Time.deltaTime);
+
+
+    }
+}
